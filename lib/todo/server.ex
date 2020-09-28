c54f3745956bfc812969795a02abdea3460a4c99
Beth Jackson <Mbeth.ashton@gmail.com>
2020-09-28 13:05:10 -0400

    10.5

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -13,17 +13,28 @@ defmodule Todo.Server do
   def entries(todo_server, date) do
     GenServer.call(todo_server, {:entries, date})
   end
+  
+  @expiry_idle_timeout :timer.seconds(10)
 
   @impl GenServer
   def init(name) do
-    {:ok, {name, Todo.Database.get(name) || Todo.List.new()}}
+    IO.puts("Starting to-do server for #{name}.")
+    {
+      :ok, 
+      {name, Todo.Database.get(name) || Todo.List.new()},
+      @expiry_idle_timeout
+    }
   end
 
   @impl GenServer
   def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
     new_list = Todo.List.add_entry(todo_list, new_entry)
     Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
+    {
+      :noreply, 
+      {name, new_list}, 
+      @expiry_idle_timeout
+    }
   end
 
   @impl GenServer
@@ -31,7 +42,8 @@ defmodule Todo.Server do
     {
       :reply,
       Todo.List.entries(todo_list, date),
-      {name, todo_list}
+      {name, todo_list}, 
+      @expiry_idle_timeout
     }
   end
   
