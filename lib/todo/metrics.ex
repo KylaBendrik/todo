d4e378a27b36654154b8dc397a163af0f19877d9
Beth Jackson <Mbeth.ashton@gmail.com>
2020-09-28 11:44:47 -0400

    10.2

--- lib/todo/metrics.ex
+++ lib/todo/metrics.ex
@@ -1,4 +1,8 @@
 defmodule Todo.Metrics do
+  use Task
+  
+  def start_link(_arg), do: Task.start_link(&loop/0)
+  
   defp loop() do
     Process.sleep(:timer.seconds(10))
     IO.inspect(collect_metrics())
