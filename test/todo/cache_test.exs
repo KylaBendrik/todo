06a31af9d8d8dd52bdf693ededcd581cb79c56d9
Beth Jackson <Mbeth.ashton@gmail.com>
2020-10-05 11:43:58 -0400

    11.1

--- test/todo/cache_test.exs
+++ test/todo/cache_test.exs
@@ -2,11 +2,10 @@ defmodule Todo.CacheTest do
   use ExUnit.Case
   
   test "server_process" do
-    {:ok, cache} = Todo.Cache.start()
-    bob_pid = Todo.Cache.server_process(cache, "bob")
+    bob_pid = Todo.Cache.server_process("bob")
 
-    assert bob_pid != Todo.Cache.server_process(cache, "alice")
-    assert bob_pid == Todo.Cache.server_process(cache, "bob")
+    assert bob_pid != Todo.Cache.server_process("alice")
+    assert bob_pid == Todo.Cache.server_process("bob")
   end
 
   test "to-do operations" do
