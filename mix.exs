57d7a574f97918c83b5fea2d39db076ded0fd965
Beth Jackson <Mbeth.ashton@gmail.com>
2020-10-05 11:44:28 -0400

    0.11.1 tagged

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Todo.MixProject do
   def project do
     [
       app: :todo,
-      version: "0.1.0",
+      version: "0.11.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
